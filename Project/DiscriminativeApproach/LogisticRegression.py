dfd6d53dead7e7f3fd113ffe6baba6d11bd28d6c
c0st0la <luca.costola98@gmail.com>
2023-09-08 10:56:55 +0200

    Updated project

--- Project/DiscriminativeApproach/LogisticRegression.py
+++ Project/DiscriminativeApproach/LogisticRegression.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 from Project import functions
-import seaborn
 import numpy
 
 if __name__ == "__main__":
+
     DTRSplitted, LTR, DTROriginal = functions.read_file("../Train.txt")
     DTESplitted, LTE, DTEOriginal = functions.read_file("../Test.txt")
     classPriorProbabilities1 = numpy.array([9 / 10, 1 / 10], dtype=float)
@@ -23,6 +23,7 @@ if __name__ == "__main__":
         DTEOriginal.std(axis=1))
 
     DOriginalNormalized = numpy.concatenate((DTROriginalNormalized, DTEOriginalNormalized), axis=1)
+    DOriginal = numpy.concatenate((DTROriginal, DTEOriginal), axis=1)
     L = numpy.concatenate((LTR, LTE), axis=0)
 
     dict1 = functions.K_fold_cross_validation_DCF(DOriginalNormalized, L, "LR",
@@ -47,7 +48,7 @@ if __name__ == "__main__":
              label=f"minDCF(effPrior={classPriorProbabilities3[1]})")
     plt.tick_params(axis='x', labelsize=6)
     plt.legend()
-    plt.savefig("LR_DCFxLambda")
+    plt.savefig("./figures/LR_Zscore_DCFxLambda")
     plt.clf()
 
     print(dict1)
@@ -63,7 +64,7 @@ if __name__ == "__main__":
     print("dict3")
     print("minDCF: ", dict3[min(dict3, key=dict3.get)])
 
-    with open("LRdati.txt", "w") as fp:
+    with open("./dati/datiLR_Zscore.txt", "w") as fp:
         fp.write("dict1 :\n")
         fp.write(str(dict1))
         fp.write('\n')
@@ -78,5 +79,59 @@ if __name__ == "__main__":
         fp.write(str(dict3))
         fp.write('\n')
         fp.write("minDCF: " + str(dict3[min(dict3, key=dict3.get)]))
-        fp.close()
 
+    ## RAW FEATURES
+
+    dict1 = functions.K_fold_cross_validation_DCF(DOriginal, L, "LR",
+                                                  numFold, classPriorProbabilities1, costs, labels, lambdValues)
+
+    dict2 = functions.K_fold_cross_validation_DCF(DOriginal, L, "LR",
+                                                  numFold, classPriorProbabilities2, costs, labels, lambdValues)
+
+    dict3 = functions.K_fold_cross_validation_DCF(DOriginal, L, "LR",
+                                                  numFold, classPriorProbabilities3, costs, labels, lambdValues)
+
+    dict1 = {keys[i]: list(dict1.values())[i] for i in range(len(list(dict1.keys())))}
+    dict2 = {keys[i]: list(dict2.values())[i] for i in range(len(list(dict2.keys())))}
+    dict3 = {keys[i]: list(dict3.values())[i] for i in range(len(list(dict3.keys())))}
+
+    plt.figure()
+    plt.plot([var for var in list(dict1.keys())], [var for var in list(dict1.values())],
+             label=f"minDCF(effPrior={classPriorProbabilities1[1]})")
+    plt.plot([var for var in list(dict2.keys())], [var for var in list(dict2.values())],
+             label=f"minDCF(effPrior={classPriorProbabilities2[1]})")
+    plt.plot([var for var in list(dict3.keys())], [var for var in list(dict3.values())],
+             label=f"minDCF(effPrior={classPriorProbabilities3[1]})")
+    plt.tick_params(axis='x', labelsize=6)
+    plt.legend()
+    plt.savefig("./figures/LR_Raw_DCFxLambda")
+    plt.clf()
+
+    print(dict1)
+    print(dict2)
+    print(dict3)
+
+    print("dict1")
+    print("minDCF: ", dict1[min(dict1, key=dict1.get)])
+
+    print("dict2")
+    print("minDCF: ", dict2[min(dict2, key=dict2.get)])
+
+    print("dict3")
+    print("minDCF: ", dict3[min(dict3, key=dict3.get)])
+
+    with open("./dati/datiLR_Raw.txt", "w") as fp:
+        fp.write("dict1 :\n")
+        fp.write(str(dict1))
+        fp.write('\n')
+        fp.write("minDCF: " + str(dict1[min(dict1, key=dict3.get)]))
+        fp.write('\n')
+        fp.write("dict2 :")
+        fp.write(str(dict2))
+        fp.write('\n')
+        fp.write("minDCF: " + str(dict2[min(dict2, key=dict2.get)]))
+        fp.write('\n')
+        fp.write("dict3 :")
+        fp.write(str(dict3))
+        fp.write('\n')
+        fp.write("minDCF: " + str(dict3[min(dict3, key=dict3.get)]))
